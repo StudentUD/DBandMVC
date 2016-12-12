1475a3329f22c95bea0ed6d67c384ffbda41cb13
Alejandro Hernandez <usercall.hm@gmail.com>
2016-12-12 18:25:19 -0500

    add inventario

---
+++ src/model/inventario/inventarioDao.java
@@ -0,0 +1,19 @@
+package model.inventario;
+
+public class inventarioDao {
+	
+	
+	public void metodocompra(String factura,String numeroItem, int cantidad, String precioUnitario){
+		
+		//REGISTRAR INVETARIO  uuario logueado, fecha de sistema, documento 
+	}  
+	
+	
+	
+	public int calacularItemExistencia(String idItem){
+		
+		
+		return 0;
+	}
+
+}
